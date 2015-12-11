c92b3c92b16bd90b3af5706d72fff797b8fba4d1
zhangjie <luoyetx@gmail.com>
2015-12-11 11:09:45 +0800

    add level2

--- ex.hpp
+++ ex.hpp
@@ -9,37 +9,38 @@
 #include <opencv2/objdetect/objdetect.hpp>
 
 struct BBox {
-	int x, y;
-	int width, height;
-	cv::Rect rect;
+  int x, y;
+  int width, height;
+  cv::Rect rect;
 
-	BBox(int x, int y, int w, int h);
-	BBox(const cv::Rect &rect);
-	BBox(BBox const &other);
-	void Project(const std::vector<cv::Point2f> &absLandmark, std::vector<cv::Point2f> &relLandmark) const;
-	void ReProject(const std::vector<cv::Point2f> &realLandmark, std::vector<cv::Point2f> &absLandmark) const;
-	BBox subBBox(float left, float right, float top, float bottom) const;
+  BBox(int x, int y, int w, int h);
+  BBox(const cv::Rect &rect);
+  BBox(BBox const &other);
+  void Project(const std::vector<cv::Point2f> &absLandmark, std::vector<cv::Point2f> &relLandmark) const;
+  void ReProject(const std::vector<cv::Point2f> &realLandmark, std::vector<cv::Point2f> &absLandmark) const;
+  BBox subBBox(float left, float right, float top, float bottom) const;
 };
 
 struct FaceDetector {
-	cv::CascadeClassifier cc;
+  cv::CascadeClassifier cc;
 
-	void LoadXML(const std::string &path);
-	int DetectFace(const cv::Mat &img, std::vector<cv::Rect> &rects);
+  void LoadXML(const std::string &path);
+  int DetectFace(const cv::Mat &img, std::vector<cv::Rect> &rects);
 };
 
 struct CNN {
-	caffe::Net<float> *cnn;
+  caffe::Net<float> *cnn;
 
-	CNN(const std::string &network, const std::string &model);
-	std::vector<cv::Point2f> forward(const cv::Mat &data, const std::string &layer);
+  CNN(const std::string &network, const std::string &model);
+  std::vector<cv::Point2f> forward(const cv::Mat &data, const std::string &layer);
 };
 
 struct Landmarker {
-	CNN *F_1;
+  CNN *F;
+  CNN *level2[10];
 
-	void LoadModel(const std::string &path);
-	std::vector<cv::Point2f> DetectLandmark(const cv::Mat &img, const BBox &bbox);
+  void LoadModel(const std::string &path);
+  std::vector<cv::Point2f> DetectLandmark(const cv::Mat &img, const BBox &bbox);
 };
 
 #endif // __EX_HPP__
--- main.cpp
+++ main.cpp
@@ -9,38 +9,40 @@ using namespace std;
 using namespace caffe;
 
 void showLandmarks(Mat &image, Rect &bbox, vector<Point2f> &landmarks) {
-	Mat img;
-	image.copyTo(img);
-	rectangle(img, bbox, Scalar(0, 0, 255), 2);
-	for (int i = 0; i < landmarks.size(); i++) {
-		Point2f &point = landmarks[i];
-		circle(img, point, 2, Scalar(0, 255, 0), -1);
-	}
-	imshow("landmark", img);
-	waitKey(0);
+  Mat img;
+  image.copyTo(img);
+  rectangle(img, bbox, Scalar(0, 0, 255), 2);
+  for (int i = 0; i < landmarks.size(); i++) {
+    Point2f &point = landmarks[i];
+    circle(img, point, 2, Scalar(0, 255, 0), -1);
+  }
+  imshow("landmark", img);
+  waitKey(0);
 }
 
 int main(int argc, char *argv[]) {
-	FaceDetector fd;
-	Landmarker lder;
-	fd.LoadXML("../haarcascade_frontalface_alt.xml");
-	lder.LoadModel("../deeplandmark");
+  google::InitGoogleLogging(argv[0]);
+  google::SetLogDestination(google::LogSeverity(), "./");
+  FaceDetector fd;
+  Landmarker lder;
+  fd.LoadXML("../haarcascade_frontalface_alt.xml");
+  lder.LoadModel("../deeplandmark");
 
-	Mat image;
-	Mat gray;
-	image = imread("../test.jpg");
-	if (image.data == NULL) return -1;
-	cvtColor(image, gray, CV_BGR2GRAY);
+  Mat image;
+  Mat gray;
+  image = imread("../test.jpg");
+  if (image.data == NULL) return -1;
+  cvtColor(image, gray, CV_BGR2GRAY);
 
-	vector<Rect> bboxes;
-	fd.DetectFace(gray, bboxes);
+  vector<Rect> bboxes;
+  fd.DetectFace(gray, bboxes);
 
-	vector<Point2f> landmarks;
-	for (int i = 0; i < bboxes.size(); i++) {
-		BBox bbox_ = BBox(bboxes[i]).subBBox(0.1, 0.9, 0.2, 1);
-		landmarks = lder.DetectLandmark(gray, bbox_);
-		showLandmarks(image, bbox_.rect, landmarks);
-	}
+  vector<Point2f> landmarks;
+  for (int i = 0; i < bboxes.size(); i++) {
+    BBox bbox_ = BBox(bboxes[i]).subBBox(0.1, 0.9, 0.2, 1);
+    landmarks = lder.DetectLandmark(gray, bbox_);
+    showLandmarks(image, bbox_.rect, landmarks);
+  }
 
-	return 0;
+  return 0;
 }
